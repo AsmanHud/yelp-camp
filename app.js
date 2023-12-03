dfb98fe7ef4042b6d4dcccd87d8f1267634decab
Asman <asman.hudaykulyyev@gmail.com>
2023-12-04 01:33:07 +0200

    change secrets from hardcoded to env

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const store = MongoStore.create({
     mongoUrl: dbUrl,
     touchAfter: 24 * 60 * 60,
     crypto: {
-        secret: 'thisshouldbeabettersecret!'
+        secret: process.env.SECRET
     }
 });
 
@@ -42,7 +42,7 @@ store.on('error', function (e) {
 const sessionConfig = {
     store,
     name: 'session',
-    secret: 'thisshouldbeabettersecret',
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
